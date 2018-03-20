96a1f032e5884312871dc36b21502abc5ff5e8c2
Masaya Kazama <miyan@t-p.jp>
2018-03-20 18:26:30 +0900

    Render issue body as markdown

--- components/PostItem.vue
+++ components/PostItem.vue
@@ -1,27 +1,46 @@
 <template>
-  <article>
-    <h3><nuxt-link :to="{
+  <article class="postItem">
+    <h2><nuxt-link :to="{
       name: 'posts-id',
       params: {
         id: post.number
       }
-    }">{{post.title}}</nuxt-link></h3>
+    }">{{post.title}}</nuxt-link></h2>
     <div>
-      {{post.body}}
+      <vue-markdown
+        class="marked"
+        :source="post.body"
+        :anchorAttributes="{
+          target: '_blank',
+          rel: 'noopener'
+        }"
+        @rendered="rendered"
+      />
     </div>
   </article>
 </template>
 
 <script>
+import VueMarkdown from 'vue-markdown'
 import PostItem from '~/components/PostItem.vue'
 
 export default {
+  components: {
+    VueMarkdown
+  },
   props: {
     post: { type: Object, required: true }
+  },
+  methods: {
+    rendered() {
+      console.log('rendered')
+    }
   }
 }
 </script>
 
 <style>
-
+.postItem {
+  border: 1px solid #000;
+}
 </style>
